dba3c40ebeb8015768328052aff1fac453d2e80b
orvino <91889241+orvino@users.noreply.github.com>
2022-03-13 18:42:15 -0500

    complete 13.4.4

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,58 +1,33 @@
-// Add console.log to check to see if our code is working.
+// add console.log to see if our code is working
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with a center (US) and zoom level
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
-// 13.4.2
-// An array containing each city's location, state, and population.
+// Coordinates for each point to be used in the line
+// LAX aiport to SFO San Francisco airport
+// Then I added Salt Lake City airport (SLC) and Seattle-Tacoma Int (SEA)
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
 
-  // Loop through the cities array and create one marker for each city.
-// cities.forEach(function(city) {
-//     console.log(city)
-//    });
-
-  // Get data from cities.js
-  let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.marker(city.location).addTo(map);
-});
-
-
-//  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// Add a circle to the map
-// L.circle([34.0522, -118.2437], {
-//     radius: 100
-//  }).addTo(map);
-
- // Skill Drill - add light-yellow circle with black lines with 300 meter radius of Central LA
-//  L.circleMarker([34.0522, -118.2437], {
-//     radius: 300,
-//     color: "black",
-//     fillColor: '#ffffa1'
-    
-//  }).addTo(map);
-
-//  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/dark-v10',
-//     accessToken: API_KEY
-// });
+// Create a polyline using the line coordinates and make the line red
+L.polyline(line, {
+    color: "blue",
+    weight: 4,
+    opacity: 0.5,
+    dashArray: '10, 10'
+}).addTo(map);
 
 
-
-
+// Using MAPBOX STYLES API
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 1800,
-    id: 'mapbox/streets-v11',
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
     accessToken: API_KEY
 });
 
