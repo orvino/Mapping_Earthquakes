67a1b1c4a1543e6cdd2efb70fd956e64a8320b0b
orvino <91889241+orvino@users.noreply.github.com>
2022-03-13 18:40:14 -0500

    work please

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -4,45 +4,54 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
+// 13.4.2
+// An array containing each city's location, state, and population.
 
-// // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  // Loop through the cities array and create one marker for each city.
+// cities.forEach(function(city) {
+//     console.log(city)
+//    });
+
+  // Get data from cities.js
+  let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.marker(city.location).addTo(map);
+});
+
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle to the map
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+ // Skill Drill - add light-yellow circle with black lines with 300 meter radius of Central LA
+//  L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: '#ffffa1'
+    
+//  }).addTo(map);
+
+//  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 //     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
+//     id: 'mapbox/dark-v10',
 //     accessToken: API_KEY
 // });
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
 
-// // To change the map's style, change the map id using the list of Mapbox ids below:
 
-// // mapbox/streets-v11
-// // mapbox/outdoors-v11
-// // mapbox/light-v10
-// // mapbox/dark-v10
-// // mapbox/satellite-v9
-// // mapbox/satellite-streets-v11
 
-// Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
 
-// 13.4.2
-
-// // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 1800,
     id: 'mapbox/streets-v11',
     accessToken: API_KEY
 });
