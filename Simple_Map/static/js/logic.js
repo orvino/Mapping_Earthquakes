d515d4eccbf16f2d7168d794fa3baaa04958daad
orvino <91889241+orvino@users.noreply.github.com>
2022-03-13 19:16:18 -0500

    13.6.1

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,51 +1,55 @@
-// Add console.log to check to see if our code is working.
+// add console.log to see if our code is working
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-
-// Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
-
-// // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: API_KEY
-// });
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
-
-// // To change the map's style, change the map id using the list of Mapbox ids below:
-
-// // mapbox/streets-v11
-// // mapbox/outdoors-v11
-// // mapbox/light-v10
-// // mapbox/dark-v10
-// // mapbox/satellite-v9
-// // mapbox/satellite-streets-v11
-
-// Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
-
-// 13.4.2
-
-// // We create the tile layer that will be the background of our map.
+// Having the tileLayer before accessing large datasets ensures that the map gets loaded before the data is added to it
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Still dont have the addTo(map)
+// instead, create a base layer that holds both maps
+let baseMaps = {
+    "Streets": streets,
+    "Satellite Street": satelliteStreets
+}; 
+
+// Create the map object with a center (US) and zoom level
+let map = L.map('mapid', {
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [streets]
+});
+
+// Control layers 
+// Pass our map layers into our layers control and add the layers control to the map
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto Neighborhoods GeoJSON url
+let torontoHoods = "https://raw.githubusercontent.com/nicoserrano/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
+
+let myStyle = {
+    color: "blue",
+    fillColor: "yellow",
+    weight: 1
+}
+
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data
+    L.geoJSON(data, {
+        style: myStyle,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h3>Neighborhood: " + feature.properties.AREA_NAME + "</h3>")
+        }
+    }).addTo(map);
+});
